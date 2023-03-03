a5ea301964b621bfbeb45f624ed6f71eea781ba7
Matheus <ymatheus.h@gmail.com>
2023-03-03 14:14:34 -0300

    style: :lipstick: Adjust style of search container and title components

--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,10 +1,27 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+
+  width: 50%;
+  height: 78px;
+  background-color: var(--main-color);
+  margin: 3rem auto;
+  border-radius: 0.8rem;
 `;
 
 export const Title = styled.h2`
+  margin: 0 1rem;
 
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 18px;
+
+  color: var(--white);
 `;
 
 export const Form = styled.form`
