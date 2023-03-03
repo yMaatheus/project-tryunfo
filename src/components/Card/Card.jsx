c7a6d0f7550bccfeeaab5e0da95071237acdbdf5
Matheus <ymatheus.h@gmail.com>
2023-03-03 12:44:04 -0300

    feat: :sparkles: Rename Card.jsx and add styles for Card components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as S from './styles';
 
 class Card extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
       cardAttr3, cardImage, cardRare, cardTrunfo } = this.props;
     return (
-      <section>
-        <span data-testid="name-card">{cardName}</span>
-        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <span data-testid="description-card">{cardDescription}</span>
-        <span data-testid="attr1-card">{cardAttr1}</span>
-        <span data-testid="attr2-card">{cardAttr2}</span>
-        <span data-testid="attr3-card">{cardAttr3}</span>
-        <span data-testid="rare-card">{cardRare}</span>
+      <S.Container>
+        <S.Name data-testid="name-card">{cardName}</S.Name>
+        <S.Image src={ cardImage } alt={ cardName } data-testid="image-card" />
+        <S.Description data-testid="description-card">{cardDescription}</S.Description>
+        <S.Attr data-testid="attr1-card">{cardAttr1}</S.Attr>
+        <S.Attr data-testid="attr2-card">{cardAttr2}</S.Attr>
+        <S.Attr data-testid="attr3-card">{cardAttr3}</S.Attr>
+        <S.Attr data-testid="rare-card">{cardRare}</S.Attr>
         {cardTrunfo && <span data-testid="trunfo-card">Super Trunfo</span>}
-      </section>
+      </S.Container>
     );
   }
 }
---
+++ src/components/Card/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+`;
+
+export const Name = styled.h3`
+`;
+
+export const Image = styled.img`
+
+`;
+
+export const Description = styled.p`
+
+`;
+
+export const Attr = styled.p`
+
+`;
