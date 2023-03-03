261b69625b3dae5956ce5045e4620dce5d582785
Matheus <ymatheus.h@gmail.com>
2023-03-03 14:14:07 -0300

    style: :lipstick: pdate AddCard styles to improve layout and add new components

--- src/components/AddCard/styles.js
+++ src/components/AddCard/styles.js
@@ -1,7 +1,52 @@
 import styled from 'styled-components';
 
-export const Form = styled.form`
+export const Container = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+
+  padding: 3rem 0;
+  margin: auto;
+
+  background-color: var(--white);
+  border-radius: 2%;
+  width: 50%;
+  height: 500px;
 `;
 
-export const Container = styled.div`
+export const Form = styled.form`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  height: 80%;
+`;
+
+export const FormTitle = styled.h2`
+  font-style: italic;
+  font-weight: 800;
+  font-size: 28px;
+  line-height: 29px;
+  text-transform: uppercase;
+
+  color: var(--accent-color);
+`;
+
+export const CardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 60%;
+`;
+
+export const PreviewTitle = styled.h2`
+  align-self: flex-start;
+
+  font-style: italic;
+  font-weight: 800;
+  font-size: 28px;
+  line-height: 29px;
+  text-align: center;
+  text-transform: uppercase;
+
+  color: var(--accent-color);
 `;
