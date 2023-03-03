1aea6bde9ace44fff0e116eacef5ce6e7cb0a660
Matheus <ymatheus.h@gmail.com>
2023-03-03 14:14:20 -0300

    style: :lipstick: Add styles for Cards component

---
+++ src/components/Cards/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+`;
+
+export const CardWrapper = styled.section`
+
+`;
