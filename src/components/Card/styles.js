62a9bb4460d656fb84f21f6b570c93b431404f82
Matheus <ymatheus.h@gmail.com>
2023-03-04 14:55:21 -0300

    style: :lipstick: Update Card styles to include width, background color, font style, font weight, font size, line height, and color

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+  width: 100%;
 `;
 
 export const Name = styled.h3`
+  background-color: var(--secondary-color);
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 27.2518px;
+  line-height: 28px;
+
+  color: var(--white);
 `;
 
 export const Image = styled.img`
