4eb47d2d00733d2d2eb4c9a96d87f0ad2b7dc3e9
Matheus <ymatheus.h@gmail.com>
2023-03-04 14:55:37 -0300

    style: :lipstick: Update CardWrapper styles

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -3,8 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.section`
   display: flex;
   justify-content: center;
+
+  margin-bottom: 3rem;
 `;
 
 export const CardWrapper = styled.section`
-
+  background-color: var(--main-color);
+  border: 8px solid var(--white);
+  border-radius: 10%;
+  width: 12%;
 `;
