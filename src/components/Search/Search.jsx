189d881d350dbe4ee84f33a398243acd044b3fa4
Matheus <ymatheus.h@gmail.com>
2023-03-05 18:24:55 -0300

    refactor: :recycle: Update search form labels and options for better clarity

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,35 +9,32 @@ function Search() {
 
   return (
     <S.Container>
-      <S.Title>Filtro de Busca</S.Title>
+      <S.Title>Filtros de busca</S.Title>
       <S.Form>
-        <label htmlFor="searchName">
-          <input
-            type="text"
-            id="searchName"
-            name="search"
-            value={ search }
-            onChange={ (event) => useAppStore.setState({ search: event.target.value }) }
-            data-testid="name-filter"
-          />
-        </label>
-        <label htmlFor="searchRare">
-          <select
-            id="searchRare"
-            name="searchRare"
-            value={ searchRare }
-            onChange={ (event) => (
-              useAppStore.setState({ searchRare: event.target.value })
-            ) }
-            data-testid="rare-filter"
-          >
-            <option>todas</option>
-            <option>normal</option>
-            <option>raro</option>
-            <option>muito raro</option>
-          </select>
-        </label>
-        <label htmlFor="searchTrunfo">
+        <input
+          type="text"
+          id="searchName"
+          name="search"
+          placeholder="Nome da carta"
+          value={ search }
+          onChange={ (event) => useAppStore.setState({ search: event.target.value }) }
+          data-testid="name-filter"
+        />
+        <select
+          id="searchRare"
+          name="searchRare"
+          value={ searchRare }
+          onChange={ (event) => (
+            useAppStore.setState({ searchRare: event.target.value })
+          ) }
+          data-testid="rare-filter"
+        >
+          <option>todas</option>
+          <option>normal</option>
+          <option>raro</option>
+          <option>muito raro</option>
+        </select>
+        <S.Label htmlFor="searchTrunfo">
           <input
             type="checkbox"
             id="searchTrunfo"
@@ -46,7 +43,8 @@ function Search() {
             onChange={ () => useAppStore.setState({ searchTrunfo: !searchTrunfo }) }
             data-testid="trunfo-filter"
           />
-        </label>
+          Super Trybe Trunfo
+        </S.Label>
       </S.Form>
     </S.Container>
   );
